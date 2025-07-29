d26d093640eafb293eed216fbc7f5ee053fd53e6
Darkkorn98 <darkkorn98@yahoo.com>
2025-07-28 23:12:55 -0400

    asdasd

---
+++ dylan-hyperspace-resume/src/Resume.js
@@ -0,0 +1,3 @@
+/* PLACEHOLDER – Replace with full animated JSX from Canvas */
+const DylanHyperspaceResume = () => <div>Loading résumé...</div>;
+export default DylanHyperspaceResume;
---
+++ dylan-hyperspace-resume/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import DylanHyperspaceResume from './Resume';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <DylanHyperspaceResume />
+  </React.StrictMode>
+);
